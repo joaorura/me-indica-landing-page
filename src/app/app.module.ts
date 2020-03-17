e625e61be721f7cb6ae108b62d8c0e1f2da06c16
João Messias Lima Pereira <jmlp@ic.ufal.br>
2020-03-17 16:32:37 -0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyInfoComponent } from './body-info/body-info.component';
+import { InfoComponent } from './body-info/info/info.component';
+import { FooterComponent } from './footer/footer.component';
+import { ModalListWaitComponent } from './modal-list-wait/modal-list-wait.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyInfoComponent,
+    InfoComponent,
+    FooterComponent,
+    ModalListWaitComponent
   ],
   imports: [
     BrowserModule
